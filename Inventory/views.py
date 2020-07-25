1cda46d9b72d9a14a6132a1969713ea8becfd322
Konnor <kbeaulier@wisc.edu>
2020-07-25 09:23:35 -0700

    deleting from upload

--- Inventory/views.py
+++ Inventory/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
-from django_filters.views import FilterView
-from django_filters import FilterSet
-from django_tables2.views import SingleTableMixin
-import csv, io
+import csv
+import io
+
 from django.contrib import messages
+from django.shortcuts import render
+from django_filters import FilterSet
+from django_filters.views import FilterView
+from django_tables2.views import SingleTableMixin
+from django.db.models import F
+
 from .models import Card
 from .tables import CardTable
 
@@ -47,7 +51,7 @@ def upload(request):
         io_string = io.StringIO(data_set)
         for column in csv.reader(io_string, delimiter=',', quotechar="|"):
             _, created = Card.objects.update_or_create(
-                name=column[0],
+                name=(column[0].replace(';', ',')),
                 edition=column[1],
                 foil=column[2],
                 quantity=column[3],
@@ -57,15 +61,24 @@ def upload(request):
     if delete_csv_file is not None:
         if not delete_csv_file.name.endswith('.csv'):
             messages.error(request, 'THIS IS NOT A CSV FILE')
-        data_set = delete_csv_file.read().decode('UTF-8')
+        data_set = delete_csv_file.read().decode('UTF-8-SIG')
 
         io_string = io.StringIO(data_set)
         for column in csv.reader(io_string, delimiter=',', quotechar="|"):
-            Card.objects.filter(
+            card = Card.objects.filter(
                 name=column[0],
                 edition=column[1],
-                foil=column[2]
-            ).delete()
+                foil=column[2],
+                bag=column[4]
+            )
+            if card.count() > 0:
+                selected_card = card[0]
+                if selected_card.quantity <= int(column[3]):
+                    selected_card.delete()
+                else:
+                    selected_card.quantity = F('quantity') - column[3]
+                    selected_card.save()
+
 
     context = {}
     return render(request, template, context)
